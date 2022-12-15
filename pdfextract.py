e17cfab0613ff6d8f837cc7783d0a7a4e5111948
vignesh chari <vigneshvangala@gmail.com>
2022-12-15 22:39:19 +0530

    extract images

--- pdfextract.py
+++ pdfextract.py
@@ -15,11 +15,36 @@ import fitz
 #         pix1.writePNG(f'{xref}.png')
 #     pix=None
 # print(len(image_list),'detected')
-file=fitz.open("mgu.pdf")
-# page=len(file)
-for page in file:
-    pix=page.get_pixmap()
-    pix.save("page-%i.png" % page.number)
 
-    
+#convert pdf to image
+def convert_to_img():
+    file=fitz.open("mgu.pdf")
+    # page=len(file)
+    for page in file:
+        pix=page.get_pixmap()
+        pix.save("page-%i.png" % page.number)
+def getimg_text():
+    file=fitz.open("pdf.pdf")
+    # for pageNumber,page in enumerate(file.pages(),start=1):
+    #     text=page.getText()
+    #     txt=open("report_page_{}.txt",'a'.format(pageNumber))
+    #     txt.writelines(text)
+    #     txt.close()
 
+    for imgNumber,img in enumerate(file.pages(),start=1):
+        xref=img[0]
+        pix=fitz.Pixmap(file,xref)
+        if pix.n>4:
+            pix=fitz.Pixmap(fitz.csRGB,pix)
+        pix._writeIMG("image_page_{}.img".format(imgNumber))
+
+from PyPDF2 import PdfReader
+
+def getimage():
+    file_name="pdf.pdf"
+    reader = PdfReader(file_name)
+    for page in reader.pages:
+        for image in page.images:
+            with open(image.name, "wb") as fp:
+                fp.write(image.data)
+    print("images extrated from the {}".format(file_name))
